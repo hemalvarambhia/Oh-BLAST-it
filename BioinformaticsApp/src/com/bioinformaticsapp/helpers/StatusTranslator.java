87fc3c95b6ebbcc2884cecbac5a99045fa2ae87b
Hemal Varambhia <h.n.varambhia@gmail.com>
2013-01-01 15:08:52 +0000

    Introduced concept of search status and translation of it to a query status

---
+++ BioinformaticsApp/src/com/bioinformaticsapp/helpers/StatusTranslator.java
@@ -0,0 +1,14 @@
+package com.bioinformaticsapp.helpers;
+
+import com.bioinformaticsapp.models.BLASTQuery.Status;
+import com.bioinformaticsapp.web.SearchStatus;
+
+public class StatusTranslator {
+
+	public Status translate(SearchStatus statusOfSearch){
+		Status statusOfQuery = Status.UNSURE;
+		
+		return statusOfQuery;
+	}
+	
+}
---
+++ BioinformaticsApp/src/com/bioinformaticsapp/web/SearchStatus.java
@@ -0,0 +1,7 @@
+package com.bioinformaticsapp.web;
+
+public enum SearchStatus {
+	
+	RUNNING, UNSURE, FINISHED, ERROR, NOT_FOUND;
+	
+}
