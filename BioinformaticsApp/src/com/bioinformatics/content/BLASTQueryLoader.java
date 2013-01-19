1a6d8ca520eaee7026720967c8d290064bb636ae
Hemal Varambhia <h.n.varambhia@gmail.com>
2013-01-19 10:50:28 +0000

    Loader to load queries. Tests for it run and pass!

---
+++ BioinformaticsApp/src/com/bioinformatics/content/BLASTQueryLoader.java
@@ -0,0 +1,44 @@
+package com.bioinformatics.content;
+
+import java.util.List;
+
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+
+import com.bioinformaticsapp.data.BLASTQueryController;
+import com.bioinformaticsapp.models.BLASTQuery;
+
+public class BLASTQueryLoader extends AsyncTaskLoader<List<BLASTQuery>> {
+
+	public BLASTQueryLoader(Context context) {
+		super(context);
+		
+	}
+	
+	public BLASTQueryLoader(Context context, BLASTQuery.Status status){
+		super(context);
+		mStatusOfQuery = status;
+	}
+
+	@Override
+	public List<BLASTQuery> loadInBackground() {
+		// TODO Auto-generated method stub
+		BLASTQueryController queryController = new BLASTQueryController(getContext());
+		List<BLASTQuery> queries = queryController.findBLASTQueriesByStatus(mStatusOfQuery);
+		queryController.close();
+		
+		return queries;
+	}
+	
+	/* (non-Javadoc)
+	 * @see android.content.Loader#onStartLoading()
+	 */
+	@Override
+	protected void onStartLoading() {
+		super.onStartLoading();
+		forceLoad();
+	}
+
+	private BLASTQuery.Status mStatusOfQuery;
+
+}
