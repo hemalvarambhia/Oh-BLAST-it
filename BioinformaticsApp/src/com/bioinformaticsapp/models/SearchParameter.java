2f5bf7142fdc572a337832b27deef62593b1f7b2
Hemal Varambhia <h.n.varambhia@gmail.com>
2013-02-02 18:18:04 +0000

    Removed an unused import. Unit tests still pass

--- BioinformaticsApp/src/com/bioinformaticsapp/models/SearchParameter.java
+++ BioinformaticsApp/src/com/bioinformaticsapp/models/SearchParameter.java
@@ -2,9 +2,7 @@ package com.bioinformaticsapp.models;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 
 public class SearchParameter implements Serializable{
