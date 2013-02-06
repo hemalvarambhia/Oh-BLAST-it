21841caa5657c833bff86a9f80ff2baa70adf66b
Hemal Varambhia <h.n.varambhia@gmail.com>
2013-02-06 09:01:49 +0000

    Unused import removed

--- BioinformaticsApp/src/com/bioinformaticsapp/data/SearchParametersDAO.java
+++ BioinformaticsApp/src/com/bioinformaticsapp/data/SearchParametersDAO.java
@@ -7,7 +7,6 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.util.Log;
 
 import com.bioinformaticsapp.models.BLASTQuery;
 import com.bioinformaticsapp.models.BLASTQuery.BLASTJob;
