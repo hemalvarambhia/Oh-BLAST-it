c518c527c73699e4495f1541847c244e1b258594
Hemal Varambhia <h.n.varambhia@gmail.com>
2013-02-11 07:18:14 +0000

    Retrieve all sent queries including legacy ones

--- BioinformaticsApp/src/com/bioinformaticsapp/content/BLASTQueryLoader.java
+++ BioinformaticsApp/src/com/bioinformaticsapp/content/BLASTQueryLoader.java
@@ -1,5 +1,6 @@
 package com.bioinformaticsapp.content;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import android.content.AsyncTaskLoader;
@@ -7,6 +8,7 @@ import android.content.Context;
 
 import com.bioinformaticsapp.data.BLASTQueryController;
 import com.bioinformaticsapp.models.BLASTQuery;
+import com.bioinformaticsapp.models.BLASTQuery.Status;
 
 public class BLASTQueryLoader extends AsyncTaskLoader<BLASTQuery[]> {
 
@@ -23,7 +25,18 @@ public class BLASTQueryLoader extends AsyncTaskLoader<BLASTQuery[]> {
 	@Override
 	public BLASTQuery[] loadInBackground() {
 		BLASTQueryController queryController = new BLASTQueryController(getContext());
-		List<BLASTQuery> queries = queryController.findBLASTQueriesByStatus(mStatusOfQuery);
+		
+		List<BLASTQuery> queries = new ArrayList<BLASTQuery>();
+		
+		switch(mStatusOfQuery){
+		case SUBMITTED:
+			queries = queryController.getSubmittedBLASTQueries();
+			break;
+		default:
+			queries = queryController.findBLASTQueriesByStatus(mStatusOfQuery);
+			break;
+		}
+		
 		queryController.close();
 		
 		return queries.toArray(new BLASTQuery[queries.size()]);
