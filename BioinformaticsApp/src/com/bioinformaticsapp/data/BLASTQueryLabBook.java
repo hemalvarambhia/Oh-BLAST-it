1dbe85e987618cca6fc5139d236ad5c2056d6e61
Hemal Varambhia <h.n.varambhia@gmail.com>
2013-12-28 09:12:28 +0000

    Added the save method to get the test to compile

--- BioinformaticsApp/src/com/bioinformaticsapp/data/BLASTQueryLabBook.java
+++ BioinformaticsApp/src/com/bioinformaticsapp/data/BLASTQueryLabBook.java
@@ -1,5 +1,20 @@
 package com.bioinformaticsapp.data;
 
+import com.bioinformaticsapp.models.BLASTQuery;
+
+import android.content.Context;
+
 public class BLASTQueryLabBook {
+
+	public BLASTQueryLabBook(Context context) {
+		this.context = context;
+	}
+
+	public boolean save(BLASTQuery aQuery) {
+		// TODO develop the logic for saving a BLAST query
+		return false;
+	}
+	
+	private Context context;
 
 }
