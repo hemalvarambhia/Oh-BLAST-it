3f5e5b4d2915adea0ab715ccb6ed03765e50c51b
Hemal Varambhia <h.n.varambhia@gmail.com>
2014-01-04 15:17:39 +0000

    Now saving the search parameters

--- BioinformaticsApp/src/com/bioinformaticsapp/data/BLASTQueryLabBook.java
+++ BioinformaticsApp/src/com/bioinformaticsapp/data/BLASTQueryLabBook.java
@@ -1,6 +1,10 @@
 package com.bioinformaticsapp.data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.bioinformaticsapp.models.BLASTQuery;
+import com.bioinformaticsapp.models.SearchParameter;
 
 import android.content.Context;
 
@@ -8,18 +12,29 @@ public class BLASTQueryLabBook {
 
 	public BLASTQueryLabBook(Context context) {
 		this.context = context;
-		blastQueryController = new BLASTQueryController(this.context);
 	}
 
 	public BLASTQuery save(BLASTQuery aQuery) {
+		blastQueryController = new BLASTQueryController(context);
+		searchParameterController = new SearchParameterController(context);
 		long queryPrimaryKey = blastQueryController.save(aQuery);
+		blastQueryController.close();
 		BLASTQuery savedQuery = (BLASTQuery)aQuery.clone();
 		savedQuery.setPrimaryKeyId(queryPrimaryKey);
 		
+		List<SearchParameter> parameters = new ArrayList<SearchParameter>();
+		for(SearchParameter parameter: aQuery.getAllParameters()){
+			parameter.setBlastQueryId(queryPrimaryKey);
+			searchParameterController.save(parameter);
+			parameters.add(parameter);
+		}
+		aQuery.updateAllParameters(parameters);
+		searchParameterController.close();
 		return savedQuery;
 	}
 	
 	private Context context;
 	private BLASTQueryController blastQueryController;
-
+	private SearchParameterController searchParameterController;
+	
 }
