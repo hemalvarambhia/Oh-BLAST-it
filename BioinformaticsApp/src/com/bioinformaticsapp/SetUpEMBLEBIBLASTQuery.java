f2d16d58d435b1c6505f2208e184bf1f0251f1a6
Hemal Varambhia <h.n.varambhia@gmail.com>
2014-09-07 16:33:46 +0100

    renamed the fields to remove the m-prefix

--- BioinformaticsApp/src/com/bioinformaticsapp/SetUpEMBLEBIBLASTQuery.java
+++ BioinformaticsApp/src/com/bioinformaticsapp/SetUpEMBLEBIBLASTQuery.java
@@ -19,36 +19,10 @@ import android.widget.Toast;
 import com.bioinformaticsapp.domain.SearchParameter;
 import com.bioinformaticsapp.text.DNASymbolFilter;
 
-/**
- * This activity allows a user to set up a BLAST query for the EMBL EBI BLAST
- * web service
- * @author Hemal N Varambhia
- *
- */
 public class SetUpEMBLEBIBLASTQuery extends SetUpBLASTQuery {
-
-	private EditText mSequenceEditor;
-	
-	private Spinner mProgramSpinner;
-	
-	private Spinner mDatabaseSpinner;
-	
-	private Spinner mScoreSpinner;
-	
-	private Spinner mExpThresholdSpinner;
-	
-	private EditText mEmailEditor;
-	
-	private Spinner mMatchMismatchSpinner;
-	
-	@SuppressWarnings("unused")
-	private static final String TAG = "SetupBLASTQueryActivity";
-
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		
+	protected void onCreate(Bundle savedInstanceState) {		
 		super.onCreate(savedInstanceState);
-		
 		setContentView(R.layout.embl_ebi_blast_query_form);
 		
 		if(isLegacyQuery()){
@@ -59,31 +33,26 @@ public class SetUpEMBLEBIBLASTQuery extends SetUpBLASTQuery {
 			query.setSearchParameter("email", "");
 		}
 		
-		mSequenceEditor = (EditText)findViewById(R.id.embl_sequence_editor);
-		mSequenceEditor.setFilters(new InputFilter[]{ new DNASymbolFilter() });
-		mProgramSpinner = (Spinner)findViewById(R.id.blastqueryentry_program_spinner);
-		mDatabaseSpinner = (Spinner)findViewById(R.id.blastqueryentry_database_spinner);
-		mScoreSpinner = (Spinner)findViewById(R.id.blastqueryentry_score_spinner);
-		mExpThresholdSpinner = (Spinner)findViewById(R.id.blastqueryentry_expthreshold_spinner);
-		mMatchMismatchSpinner = (Spinner)findViewById(R.id.ebi_match_mismatch_score_spinner);
-		mProgressDialog = new ProgressDialog(this, ProgressDialog.STYLE_SPINNER);
-		mProgressDialog.setCancelable(false);
-		mProgressDialog.setCanceledOnTouchOutside(false);
-		mEmailEditor = (EditText)findViewById(R.id.embl_send_to_email);
+		sequenceEditor = (EditText)findViewById(R.id.embl_sequence_editor);
+		sequenceEditor.setFilters(new InputFilter[]{ new DNASymbolFilter() });
+		programSpinner = (Spinner)findViewById(R.id.blastqueryentry_program_spinner);
+		databaseSpinner = (Spinner)findViewById(R.id.blastqueryentry_database_spinner);
+		scoreSpinner = (Spinner)findViewById(R.id.blastqueryentry_score_spinner);
+		expThresholdSpinner = (Spinner)findViewById(R.id.blastqueryentry_expthreshold_spinner);
+		matchMismatchSpinner = (Spinner)findViewById(R.id.ebi_match_mismatch_score_spinner);
+		progressDialog = new ProgressDialog(this, ProgressDialog.STYLE_SPINNER);
+		progressDialog.setCancelable(false);
+		progressDialog.setCanceledOnTouchOutside(false);
+		emailEditor = (EditText)findViewById(R.id.embl_send_to_email);
 		attachListeners();
-		
 	}
 
-	/* (non-Javadoc)
-	 * @see android.app.Activity#onPause()
-	 */
 	@Override
 	protected void onPause() {
-		
 		super.onPause();
-		if(mProgressDialog !=null){
-			if(mProgressDialog.isShowing()){
-				mProgressDialog.dismiss();
+		if(progressDialog !=null){
+			if(progressDialog.isShowing()){
+				progressDialog.dismiss();
 			}
 		}
 		
@@ -96,7 +65,6 @@ public class SetUpEMBLEBIBLASTQuery extends SetUpBLASTQuery {
 		default:
 			break;
 		}
-		
 	}
 	
 	protected void setUpScreenWithInitialValues(){
@@ -106,117 +74,99 @@ public class SetUpEMBLEBIBLASTQuery extends SetUpBLASTQuery {
 		String[] blastProgramsOptions = appResources.getStringArray(R.array.blast_program_options);
 		List<String> listOfBLASTPrograms = Arrays.asList(blastProgramsOptions);
 		int programPosition = listOfBLASTPrograms.indexOf(program);
-		mProgramSpinner.setSelection(programPosition);
+		programSpinner.setSelection(programPosition);
 		
 		SearchParameter database = query.getSearchParameter("database");
 		String[] databaseOptions = appResources.getStringArray(R.array.blast_database_options);
 		List<String> listOfDatabases = Arrays.asList(databaseOptions);
 		int databasePosition = listOfDatabases.indexOf(database.getValue());
-		mDatabaseSpinner.setSelection(databasePosition);
+		databaseSpinner.setSelection(databasePosition);
 		
 		SearchParameter expThreshold = query.getSearchParameter("exp_threshold");
 		String[] expThresholdOptions = appResources.getStringArray(R.array.exp_threshold_options);
 		List<String> listOfExpThresholds = Arrays.asList(expThresholdOptions);
 		int expThresholdPosition = listOfExpThresholds.indexOf(expThreshold.getValue());
-		mExpThresholdSpinner.setSelection(expThresholdPosition);
+		expThresholdSpinner.setSelection(expThresholdPosition);
 		
 		SearchParameter score = query.getSearchParameter("score");
 		String[] scoreOptions = appResources.getStringArray(R.array.blastqueryentry_score_options);
 		List<String> listOfScores = Arrays.asList(scoreOptions);
 		int scorePosition = listOfScores.indexOf(score.getValue());
-		mScoreSpinner.setSelection(scorePosition);
+		scoreSpinner.setSelection(scorePosition);
 		
 		SearchParameter matchMismatchScore = query.getSearchParameter("match_mismatch_score");
 		String[] matchMismatchScoreOptions = appResources.getStringArray(R.array.ebi_match_mismatch_score_options);
 		List<String> listOfMatchMismatchScores = Arrays.asList(matchMismatchScoreOptions);
 		int matchMismatchScorePosition = listOfMatchMismatchScores.indexOf(matchMismatchScore.getValue());
-		mMatchMismatchSpinner.setSelection(matchMismatchScorePosition);
+		matchMismatchSpinner.setSelection(matchMismatchScorePosition);
 		
 		if(query.getSequence() == null || query.getSequence().isEmpty()){
-			mSequenceEditor.setHint("Enter a sequence");
+			sequenceEditor.setHint("Enter a sequence");
 		}else{
-			mSequenceEditor.setText(query.getSequence());
+			sequenceEditor.setText(query.getSequence());
 		}
 		
 		SearchParameter email = query.getSearchParameter("email");
 		if(email.getValue() == null || email.getValue().isEmpty()){
-			mEmailEditor.setHint("Enter an e-mail address");		
+			emailEditor.setHint("Enter an e-mail address");		
 		}else{
-			mEmailEditor.setText(email.getValue());
+			emailEditor.setText(email.getValue());
 		}
 		
 	}
 	
 	private boolean isLegacyQuery(){
-		
 		SearchParameter matchMismatchScore = query.getSearchParameter("match_mismatch_score");
-		
 		SearchParameter email = query.getSearchParameter("email");
-		
-		
-		return matchMismatchScore == null && email == null;
-		
+		return matchMismatchScore == null && email == null;		
 	}
 	
 	private void attachListeners(){
-		
-		mProgramSpinner.setOnItemSelectedListener(new Spinner.OnItemSelectedListener(){
-
+		programSpinner.setOnItemSelectedListener(new Spinner.OnItemSelectedListener(){
 			public void onItemSelected(AdapterView<?> arg0, View arg1,
 					int arg2, long arg3) {
-				query.setBLASTProgram(mProgramSpinner.getSelectedItem().toString());
-				
+				query.setBLASTProgram(programSpinner.getSelectedItem().toString());
 			}
 
 			public void onNothingSelected(AdapterView<?> arg0) {
 				
 			}
-			
 		});
-		mSequenceEditor.addTextChangedListener(new TextWatcher() {
-			
+		
+		sequenceEditor.addTextChangedListener(new TextWatcher() {	
 			public void onTextChanged(CharSequence s, int start, int before, int count) {
 				
 			}
 			
 			public void beforeTextChanged(CharSequence s, int start, int count,
 					int after) {
-				
-				
+								
 			}
 			
 			public void afterTextChanged(Editable s) {
-				
 				query.setSequence(s.toString());
-				
 			}
 		});
 		
-		mEmailEditor.addTextChangedListener(new TextWatcher() {
-			
+		emailEditor.addTextChangedListener(new TextWatcher() {
 			public void onTextChanged(CharSequence s, int start, int before, int count) {
-				// Do nothing
 				
 			}
 			
 			public void beforeTextChanged(CharSequence s, int start, int count,
 					int after) {
-				// Do nothing
 				
 			}
 			
 			public void afterTextChanged(Editable s) {
 				query.setSearchParameter("email", s.toString());
-				
 			}
 		});
 		
-		mDatabaseSpinner.setOnItemSelectedListener(new Spinner.OnItemSelectedListener() {
-
+		databaseSpinner.setOnItemSelectedListener(new Spinner.OnItemSelectedListener() {
 			public void onItemSelected(AdapterView<?> arg0, View arg1,
 					int arg2, long arg3) {
-				query.setSearchParameter("database", mDatabaseSpinner.getSelectedItem().toString());
-				
+				query.setSearchParameter("database", databaseSpinner.getSelectedItem().toString());
 			}
 
 			public void onNothingSelected(AdapterView<?> arg0) {
@@ -224,48 +174,45 @@ public class SetUpEMBLEBIBLASTQuery extends SetUpBLASTQuery {
 			}
 		});
 		
-		mScoreSpinner.setOnItemSelectedListener(new OnItemSelectedListener(){
-
+		scoreSpinner.setOnItemSelectedListener(new OnItemSelectedListener(){
 			public void onItemSelected(AdapterView<?> arg0, View arg1,
 					int arg2, long arg3) {
-				query.setSearchParameter("score", mScoreSpinner.getSelectedItem().toString());
-				
+				query.setSearchParameter("score", scoreSpinner.getSelectedItem().toString());
 			}
 
 			public void onNothingSelected(AdapterView<?> arg0) {
 								
 			}
-			
 		});
 		
-		mExpThresholdSpinner.setOnItemSelectedListener(new Spinner.OnItemSelectedListener(){
-
+		expThresholdSpinner.setOnItemSelectedListener(new Spinner.OnItemSelectedListener(){
 			public void onItemSelected(AdapterView<?> arg0, View arg1,
 					int arg2, long arg3) {
-				query.setSearchParameter("exp_threshold", mExpThresholdSpinner.getSelectedItem().toString());
-				
+				query.setSearchParameter("exp_threshold", expThresholdSpinner.getSelectedItem().toString());
 			}
 
 			public void onNothingSelected(AdapterView<?> arg0) {
 				
 			}
-			
 		});
 		
-		mMatchMismatchSpinner.setOnItemSelectedListener(new Spinner.OnItemSelectedListener(){
-
+		matchMismatchSpinner.setOnItemSelectedListener(new Spinner.OnItemSelectedListener(){
 			public void onItemSelected(AdapterView<?> arg0, View arg1,
 					int arg2, long arg3) {
-				query.setSearchParameter("match_mismatch_score", mMatchMismatchSpinner.getSelectedItem().toString());
-				
+				query.setSearchParameter("match_mismatch_score", matchMismatchSpinner.getSelectedItem().toString());
 			}
 
 			public void onNothingSelected(AdapterView<?> arg0) {
 				
-				
-			}
-			
+			}		
 		});
 	}
-		
+	
+	private EditText sequenceEditor;
+	private Spinner programSpinner;
+	private Spinner databaseSpinner;
+	private Spinner scoreSpinner;
+	private Spinner expThresholdSpinner;
+	private EditText emailEditor;
+	private Spinner matchMismatchSpinner;
 }
--- BioinformaticsApp/src/com/bioinformaticsapp/SetUpNCBIBLASTQuery.java
+++ BioinformaticsApp/src/com/bioinformaticsapp/SetUpNCBIBLASTQuery.java
@@ -19,49 +19,30 @@ import com.bioinformaticsapp.text.DNASymbolFilter;
 
 public class SetUpNCBIBLASTQuery extends SetUpBLASTQuery {
 
-	private EditText mSequenceEditor;
-	private Spinner mProgramSpinner;
-	private Spinner mDatabaseSpinner;
-	private Spinner mWordSizeSpinner;
-	private EditText mExpThresholdEditText;
-	private Spinner mMatchMismatchScoreSpinner;
-	
-	private static final String TAG = "NCBIQuerySetUpActivity";
-	
-	
-	/* (non-Javadoc)
-	 * @see android.app.Activity#onCreate(android.os.Bundle)
-	 */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.ncbi_blast_query_form);
 		
-		mProgramSpinner = (Spinner)findViewById(R.id.ncbi_program_spinner);
-		mSequenceEditor = (EditText)findViewById(R.id.ncbi_sequence_edittext);
-		mSequenceEditor.setFilters(new InputFilter[]{ new DNASymbolFilter() });
-		mDatabaseSpinner = (Spinner)findViewById(R.id.ncbi_database_spinner);
-		mWordSizeSpinner = (Spinner)findViewById(R.id.ncbi_wordsize_spinner);
-		mExpThresholdEditText = (EditText)findViewById(R.id.ncbi_exp_threshold_edittext);
-		mMatchMismatchScoreSpinner = (Spinner)findViewById(R.id.ncbi_match_mismatch_spinner);
-		mProgressDialog = new ProgressDialog(this, ProgressDialog.STYLE_SPINNER);
-		mProgressDialog.setCancelable(false);
-		mProgressDialog.setCanceledOnTouchOutside(false);
-		attachListeners();
-		
-		
+		programSpinner = (Spinner)findViewById(R.id.ncbi_program_spinner);
+		sequenceEditor = (EditText)findViewById(R.id.ncbi_sequence_edittext);
+		sequenceEditor.setFilters(new InputFilter[]{ new DNASymbolFilter() });
+		databaseSpinner = (Spinner)findViewById(R.id.ncbi_database_spinner);
+		wordSizeSpinner = (Spinner)findViewById(R.id.ncbi_wordsize_spinner);
+		expThresholdEditText = (EditText)findViewById(R.id.ncbi_exp_threshold_edittext);
+		matchMismatchScoreSpinner = (Spinner)findViewById(R.id.ncbi_match_mismatch_spinner);
+		progressDialog = new ProgressDialog(this, ProgressDialog.STYLE_SPINNER);
+		progressDialog.setCancelable(false);
+		progressDialog.setCanceledOnTouchOutside(false);
+		attachListeners();		
 	}
 
-	/* (non-Javadoc)
-	 * @see android.app.Activity#onPause()
-	 */
 	@Override
 	protected void onPause() {
-		
 		super.onPause();
-		if(mProgressDialog !=null){
-			if(mProgressDialog.isShowing()){
-				mProgressDialog.dismiss();
+		if(progressDialog !=null){
+			if(progressDialog.isShowing()){
+				progressDialog.dismiss();
 			}
 		}
 		switch(query.getStatus()){
@@ -82,109 +63,94 @@ public class SetUpNCBIBLASTQuery extends SetUpBLASTQuery {
 		String[] blastProgramsOptions = appResources.getStringArray(R.array.blast_program_options);
 		List<String> listOfBLASTPrograms = Arrays.asList(blastProgramsOptions);
 		int programPosition = listOfBLASTPrograms.indexOf(program);
-		mProgramSpinner.setSelection(programPosition);
+		programSpinner.setSelection(programPosition);
 		
 		String database = query.getSearchParameter("database").getValue();
 		String[] databaseOptions = appResources.getStringArray(R.array.ncbi_database_options);
 		List<String> listOfNCBIDatabases = Arrays.asList(databaseOptions);
 		int databasePosition = listOfNCBIDatabases.indexOf(database);
-		mDatabaseSpinner.setSelection(databasePosition);
+		databaseSpinner.setSelection(databasePosition);
 		
 		String wordSize = query.getSearchParameter("word_size").getValue();
 		String[] wordSizeOptions = appResources.getStringArray(R.array.ncbi_word_size_options);
 		List<String> listOfWordSizes = Arrays.asList(wordSizeOptions);
 		int wordSizePosition = listOfWordSizes.indexOf(wordSize);
-		mWordSizeSpinner.setSelection(wordSizePosition);
+		wordSizeSpinner.setSelection(wordSizePosition);
 		
 		String expThreshold = query.getSearchParameter("exp_threshold").getValue();
-		mExpThresholdEditText.setText(expThreshold);
+		expThresholdEditText.setText(expThreshold);
 		
 		String matchMisMatchScore = query.getSearchParameter("match_mismatch_score").getValue();
 		String[] matchMisMatchScoreOptions = appResources.getStringArray(R.array.ncbi_match_mismatch_score_options);
 		List<String> matchMisMatchScores = Arrays.asList(matchMisMatchScoreOptions);
 		int matchMismatchScorePosition = matchMisMatchScores.indexOf(matchMisMatchScore);
-		mMatchMismatchScoreSpinner.setSelection(matchMismatchScorePosition);
+		matchMismatchScoreSpinner.setSelection(matchMismatchScorePosition);
 		
 		if(query.getSequence() == null || query.getSequence().isEmpty()){
-			
-			mSequenceEditor.setHint("Enter a sequence");
-		
+			sequenceEditor.setHint("Enter a sequence");
 		}else{
-			
-			mSequenceEditor.setText(query.getSequence());
-		
+			sequenceEditor.setText(query.getSequence());
 		}
-		
 	}
 	
 	private void attachListeners(){
-		mProgramSpinner.setOnItemSelectedListener(new Spinner.OnItemSelectedListener(){
+		programSpinner.setOnItemSelectedListener(new Spinner.OnItemSelectedListener(){
 
 			public void onItemSelected(AdapterView<?> arg0, View arg1,
 					int arg2, long arg3) {
-				query.setBLASTProgram(mProgramSpinner.getSelectedItem().toString());
+				query.setBLASTProgram(programSpinner.getSelectedItem().toString());
 				
 			}
 
 			public void onNothingSelected(AdapterView<?> arg0) {
-				//Do nothing
+				
 			}
-			
 		});
 		
-		mDatabaseSpinner.setOnItemSelectedListener(new Spinner.OnItemSelectedListener(){
-
+		databaseSpinner.setOnItemSelectedListener(new Spinner.OnItemSelectedListener(){
 			public void onItemSelected(AdapterView<?> arg0, View arg1,
 					int arg2, long arg3) {
-				query.setSearchParameter("database", mDatabaseSpinner.getSelectedItem().toString());
+				query.setSearchParameter("database", databaseSpinner.getSelectedItem().toString());
 				
 			}
 
 			public void onNothingSelected(AdapterView<?> arg0) {
-				//Do nothing
+				
 			}
-			
 		});
 		
-		mMatchMismatchScoreSpinner.setOnItemSelectedListener(new Spinner.OnItemSelectedListener(){
+		matchMismatchScoreSpinner.setOnItemSelectedListener(new Spinner.OnItemSelectedListener(){
+			public void onItemSelected(AdapterView<?> arg0, View arg1,
+					int arg2, long arg3) {
+				query.setSearchParameter("match_mismatch_score", matchMismatchScoreSpinner.getSelectedItem().toString());
+			}
+
+			public void onNothingSelected(AdapterView<?> arg0) {
+				
+			}
+		});
+		
+		wordSizeSpinner.setOnItemSelectedListener(new Spinner.OnItemSelectedListener(){
 
 			public void onItemSelected(AdapterView<?> arg0, View arg1,
 					int arg2, long arg3) {
-				query.setSearchParameter("match_mismatch_score", mMatchMismatchScoreSpinner.getSelectedItem().toString());
+				query.setSearchParameter("word_size", wordSizeSpinner.getSelectedItem().toString());
 				
 			}
 
 			public void onNothingSelected(AdapterView<?> arg0) {
-				// Do nothing
-				
+					
 			}
-			
 		});
 		
-		mWordSizeSpinner.setOnItemSelectedListener(new Spinner.OnItemSelectedListener(){
-
-			public void onItemSelected(AdapterView<?> arg0, View arg1,
-					int arg2, long arg3) {
-				query.setSearchParameter("word_size", mWordSizeSpinner.getSelectedItem().toString());
-				
-			}
-
-			public void onNothingSelected(AdapterView<?> arg0) {
-				// Do nothing
-				
-			}
-			
-		});
-		
-		mSequenceEditor.addTextChangedListener(new TextWatcher() {
-			
+		sequenceEditor.addTextChangedListener(new TextWatcher() {
 			public void onTextChanged(CharSequence s, int start, int before, int count) {
-				//Do nothing
+				
 			}
 			
 			public void beforeTextChanged(CharSequence s, int start, int count,
 					int after) {				
-				//Do nothing
+				
 			}
 			
 			public void afterTextChanged(Editable s) {
@@ -192,25 +158,26 @@ public class SetUpNCBIBLASTQuery extends SetUpBLASTQuery {
 			}
 		});
 		
-		mExpThresholdEditText.addTextChangedListener(new TextWatcher() {
-			
+		expThresholdEditText.addTextChangedListener(new TextWatcher() {
 			public void onTextChanged(CharSequence s, int start, int before, int count) {
-				// Do nothing
 				
 			}
 			
 			public void beforeTextChanged(CharSequence s, int start, int count,
 					int after) {
-				// Do nothing
 				
 			}
 			
 			public void afterTextChanged(Editable s) {
-				
-				query.setSearchParameter("exp_threshold", s.toString());
-				
+				query.setSearchParameter("exp_threshold", s.toString());			
 			}
 		});
 	}
-	
+
+	private EditText sequenceEditor;
+	private Spinner programSpinner;
+	private Spinner databaseSpinner;
+	private Spinner wordSizeSpinner;
+	private EditText expThresholdEditText;
+	private Spinner matchMismatchScoreSpinner;
 }
